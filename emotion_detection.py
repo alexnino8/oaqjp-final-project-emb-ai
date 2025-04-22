298ad62bc69f91def4833c9dd3dd395d15e76fbd
alexnino8 <alopeznino98@gmail.com>
2025-04-21 22:24:03 -0400

    changes formatting of output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -14,6 +15,34 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, headers=headers, json=payload)
 
-    return response.text
 
-    
+    result = json.loads(response.text)
+
+    emotion_scores = result['emotionPredictions'][0]['emotion']
+
+    anger = emotion_scores.get('anger', 0)
+    disgust = emotion_scores.get('disgust', 0)
+    fear = emotion_scores.get('fear', 0)
+    joy = emotion_scores.get('joy', 0)
+    sadness = emotion_scores.get('sadness', 0)
+
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
+
+
+
